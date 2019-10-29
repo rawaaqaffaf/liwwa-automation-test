924d77d9d3c5f4f715ff7bd82fd9dcf268cf81e1
Rawa <rawa.qaffaf@gmail.com>
2019-10-29 21:16:08 +0200

    deleted chrome directory from file

--- tests.py
+++ tests.py
@@ -19,7 +19,7 @@ from basePage import SearchPageClass
 class TEST_GITHUB_SEARCH_BASE(unittest.TestCase):
 	@classmethod 
 	def setUp(self): 
-		self.driver  = webdriver.Chrome(currentdir + "/chromedriver.exe") 
+		self.driver  = webdriver.Chrome() 
 		self.driver.get("https://github.com/") 
 		print("We are on the home page")
 		self.driver.maximize_window()
