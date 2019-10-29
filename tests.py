fc9b70d675a98188106477fb026f10b8e5ebe551
Rawa <rawa.qaffaf@gmail.com>
2019-10-29 22:05:55 +0200

    test

--- tests.py
+++ tests.py
@@ -19,8 +19,8 @@ from basePage import SearchPageClass
 class TEST_GITHUB_SEARCH_BASE(unittest.TestCase):
 	@classmethod 
 	def setUp(self): 
-		self.driver  = webdriver.Chrome("/home/travis/virtualenv/python3.6.7/bin/") 
-		self.driver.get("https://github.com/")    
+		self.driver  = webdriver.Chrome(currentdir+"/chromedriver.exe")  
+		self.driver.get("https://github.com/")  
 		print("We are on the home page")  
 		self.driver.maximize_window()
  
